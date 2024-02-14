97b361ba290d7fc077249197d2a66dde64024ea8
mahade <mahade.adib45@gmail.com>
2024-02-14 18:47:25 +0600

    added functions with js

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,27 @@
+//function play(){
+    //step-1: hide the home screen. to hide the screen add the class hidden to the home section
+//    const homeSection = document.getElementById("home-screen");
+//    homeSection.classList.add('hidden')
+
+    //step-2: show the playground.
+//   const playgroundSection = document.getElementById('play-ground')
+//    playgroundSection.classList.remove('hidden')
+//}
+
+function continueGame(){
+    //step-1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+
+    //step-2: set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    //set background color
+    setBackgroundColorById(alphabet);
+}
+
 function play(){
-    console.log('play start now');
+    hideElementId('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }
