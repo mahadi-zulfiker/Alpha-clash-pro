f881acc12c764ab5e1ef8ced1e8484731fb4461b
mahade <mahade.adib45@gmail.com>
2024-02-15 08:06:07 +0600

    added keyboard functio

--- scripts/utility.js
+++ scripts/utility.js
@@ -13,12 +13,17 @@ function setBackgroundColorById(elementId){
     element.classList.add('bg-orange-400')
 }
 
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
 function getARandomAlphabet(){
     //get or create an alphabet array
-    const  alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    const  alphabetString = "abcdefghijklmnopqrstuvwxyz/";
     const alphabets = alphabetString.split('');
-    // get a random index between 0-25
-    const randomNumber = Math.random() * 25;
+    // get a random index between 0-26
+    const randomNumber = Math.random() * 26;
     const index = Math.round(randomNumber);
     
     const alphabet = alphabets[index];
